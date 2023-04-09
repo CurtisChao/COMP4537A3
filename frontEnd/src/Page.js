fde51481e9a7fdea01aea312cc74fb795a749168
CurtisChao <cchao38@my.bcit.ca>
2023-04-08 21:24:44 -0700

    added helper function to get img urls

--- frontEnd/src/Page.js
+++ frontEnd/src/Page.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
-function page({ currentPokemons, currentPage }) {
+function Page({ currentPokemons, currentPage }) {
+
+const getUrl = (id) => {
+    if (id < 10)
+        return `https://github.com/fanzeyi/pokemon.json/raw/master/images/00${id}.png`;
+    if (id < 100)
+        return `https://github.com/fanzeyi/pokemon.json/raw/master/images/0${id}.png`;
+    return `https://github.com/fanzeyi/pokemon.json/raw/master/images/${id}.png`;
+    };
+
   return (
     <div>
       <h1>
@@ -8,11 +17,15 @@ function page({ currentPokemons, currentPage }) {
       </h1>
       {
         currentPokemons.map(item => {
-          return <div>  {item.name.english} id is {item.id} </div>
+          return <div>  {item.name.english} id is {item.id}               
+          <img
+          src={getUrl(item.id)}
+          alt={item.name.english}/>
+          </div>
         })
       }
     </div>
   )
 }
 
-export default page
+export default Page
--- frontEnd/src/Pagination.js
+++ frontEnd/src/Pagination.js
@@ -1,38 +1,42 @@
-import React from 'react'
+import React from "react";
 
-function pagination({ numberOfPages, currentPage, setCurrentPage }) {
-  const pageNumbers = []
-  for (let i = 1; i <= numberOfPages; i++) {
-    pageNumbers.push(i)
-  }
+function Pagination({ numberOfPages, currentPage, setCurrentPage }) {
+  const pageNums = Array.from({ length: numberOfPages }, (_, i) => i + 1);
+
   const nextPage = () => {
-    if (currentPage !== numberOfPages) setCurrentPage(currentPage + 1)
-  }
+    if (currentPage !== numberOfPages) setCurrentPage(currentPage + 1);
+  };
   const prevPage = () => {
-    if (currentPage !== 1) setCurrentPage(currentPage - 1)
-  }
-
-
+    if (currentPage !== 1) setCurrentPage(currentPage - 1);
+  };
 
   return (
     <div>
-      <button onClick={prevPage}>prev </button>
+      {currentPage !== 1 && <button onClick={prevPage}>prev </button>}
 
-      {
-        pageNumbers.map(number => {
-          return (<>
-            <button onClick={() => setCurrentPage(number)}>
-              {number}
-            </button>
-          </>)
-        })
-      }
+      {pageNums.map((num) => {
+        if (num < currentPage + 5 && num > currentPage - 5)
+          return (
+            <>
+              <button
+                onClick={() => setCurrentPage(num)}
+                style={
+                  num === currentPage
+                    ? { fontWeight: "bold", background: "white", color: "blue" }
+                    : {}
+                }
+              >
+                {num}
+              </button>
+            </>
+          );
+      })}
 
-      <button onClick={nextPage}>
-        next
-      </button>
+      {currentPage !== numberOfPages && (
+        <button onClick={nextPage}>next</button>
+      )}
     </div>
-  )
+  );
 }
 
-export default pagination
+export default Pagination;
