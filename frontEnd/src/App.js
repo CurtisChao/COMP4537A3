b6516b76a5f958f1039b21c4332fdeff099c90a2
CurtisChao <cchao38@my.bcit.ca>
2023-04-09 03:15:04 -0700

    fixed routing in App

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -27,16 +27,24 @@ function App() {
 
   return (
 
-    <>
+    <UserContext.Provider value={auth}>
       <BrowserRouter>
-      < Page currentPokemons={currentPokemons} currentPage={currentPage} />
-      < Pagination
-        numberOfPages={numberOfPages}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossorigin="anonymous"
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={loggedIn ? <SearchPage /> : <Navigate to="/login" />}
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </BrowserRouter>
-    </>
+    </UserContext.Provider>
   )
 }
 
